03f8d0fbc18f41b980a478465e33126376f3cedd
AzumattDev <azumattgaming@gmail.com>
2021-12-21 23:52:55 -0500

    Init

---
+++ Plugin.cs
@@ -0,0 +1,122 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ItemManager;
+using ServerSync;
+
+namespace ItemManagerModTemplate
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ModTemplatePlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "ItemManagerModTemplate";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        internal static string ConnectionError = "";
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource ItemManagerModTemplateLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+            Item ironFangAxe = new("ironfang", "IronFangAxe", "IronFang");
+            ironFangAxe.Name.English("Iron Fang Axe"); // You can use this to fix the display name in code
+            ironFangAxe.Description.English("A sharp blade made of iron.");
+            ironFangAxe.Name.German("Eisenzahnaxt"); // Or add translations for other languages
+            ironFangAxe.Description.German("Eine sehr scharfe Axt, bestehend aus Eisen und Wolfszähnen.");
+            ironFangAxe.Crafting.Add("MyAmazingCraftingStation",
+                3); // Custom crafting stations can be specified as a string
+            ironFangAxe.RequiredItems.Add("Iron", 120);
+            ironFangAxe.RequiredItems.Add("WolfFang", 20);
+            ironFangAxe.RequiredItems.Add("Silver", 40);
+            ironFangAxe.RequiredUpgradeItems
+                .Add("Iron", 20); // Upgrade requirements are per item, even if you craft two at the same time
+            ironFangAxe.RequiredUpgradeItems.Add("Silver",
+                10); // 10 Silver: You need 10 silver for level 2, 20 silver for level 3, 30 silver for level 4
+            ironFangAxe.CraftAmount = 2; // We really want to dual wield these
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                ItemManagerModTemplateLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                ItemManagerModTemplateLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                ItemManagerModTemplateLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<bool>? _serverConfigLocked;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        #endregion
+    }
+}
---
+++ VersionHandshake.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace ItemManagerModTemplate
+{
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.OnNewConnection))]
+    public static class RegisterAndCheckVersion
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            // Register version check call
+            ModTemplatePlugin.ItemManagerModTemplateLogger.LogDebug("Registering version RPC handler");
+            peer.m_rpc.Register($"{ModTemplatePlugin.ModName}_VersionCheck",
+                new Action<ZRpc, ZPackage>(RpcHandlers.RPC_PvPAlwaysOn_Version));
+
+            // Make calls to check versions
+            ModTemplatePlugin.ItemManagerModTemplateLogger.LogInfo("Invoking version check");
+            ZPackage zpackage = new();
+            zpackage.Write(ModTemplatePlugin.ModVersion);
+            peer.m_rpc.Invoke($"{ModTemplatePlugin.ModName}_VersionCheck", zpackage);
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_PeerInfo))]
+    public static class VerifyClient
+    {
+        private static bool Prefix(ZRpc rpc, ZPackage pkg, ref ZNet __instance)
+        {
+            if (!__instance.IsServer() || RpcHandlers.ValidatedPeers.Contains(rpc)) return true;
+            // Disconnect peer if they didn't send mod version at all
+            ModTemplatePlugin.ItemManagerModTemplateLogger.LogWarning(
+                $"Peer ({rpc.m_socket.GetHostName()}) never sent version or couldn't due to previous disconnect, disconnecting");
+            rpc.Invoke("Error", 3);
+            return false; // Prevent calling underlying method
+        }
+
+        private static void Postfix(ZNet __instance)
+        {
+            ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.instance.GetServerPeerID(), "RequestAdminSync",
+                new ZPackage());
+        }
+    }
+
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.ShowConnectError))]
+    public class ShowConnectionError
+    {
+        private static void Postfix(FejdStartup __instance)
+        {
+            if (__instance.m_connectionFailedPanel.activeSelf)
+                __instance.m_connectionFailedError.text += "\n" + ModTemplatePlugin.ConnectionError;
+        }
+    }
+
+    [HarmonyPatch(typeof(ZNet), nameof(ZNet.Disconnect))]
+    public static class RemoveDisconnectedPeerFromVerified
+    {
+        private static void Prefix(ZNetPeer peer, ref ZNet __instance)
+        {
+            if (!__instance.IsServer()) return;
+            // Remove peer from validated list
+            ModTemplatePlugin.ItemManagerModTemplateLogger.LogInfo(
+                $"Peer ({peer.m_rpc.m_socket.GetHostName()}) disconnected, removing from validated list");
+            _ = RpcHandlers.ValidatedPeers.Remove(peer.m_rpc);
+        }
+    }
+
+    public static class RpcHandlers
+    {
+        public static readonly List<ZRpc> ValidatedPeers = new();
+
+        public static void RPC_PvPAlwaysOn_Version(ZRpc rpc, ZPackage pkg)
+        {
+            string? version = pkg.ReadString();
+            ModTemplatePlugin.ItemManagerModTemplateLogger.LogInfo("Version check, local: " +
+                                                                   ModTemplatePlugin.ModVersion +
+                                                                   ",  remote: " + version);
+            if (version != ModTemplatePlugin.ModVersion)
+            {
+                ModTemplatePlugin.ConnectionError =
+                    $"{ModTemplatePlugin.ModName} Installed: {ModTemplatePlugin.ModVersion}\n Needed: {version}";
+                if (!ZNet.instance.IsServer()) return;
+                // Different versions - force disconnect client from server
+                ModTemplatePlugin.ItemManagerModTemplateLogger.LogWarning(
+                    $"Peer ({rpc.m_socket.GetHostName()}) has incompatible version, disconnecting");
+                rpc.Invoke("Error", 3);
+            }
+            else
+            {
+                if (!ZNet.instance.IsServer())
+                {
+                    // Enable mod on client if versions match
+                    ModTemplatePlugin.ItemManagerModTemplateLogger.LogInfo("Received same version from server!");
+                }
+                else
+                {
+                    // Add client to validated list
+                    ModTemplatePlugin.ItemManagerModTemplateLogger.LogInfo(
+                        $"Adding peer ({rpc.m_socket.GetHostName()}) to validated list");
+                    ValidatedPeers.Add(rpc);
+                }
+            }
+        }
+    }
+}
