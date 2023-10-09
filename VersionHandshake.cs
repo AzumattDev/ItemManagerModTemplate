1ac351135ce5c106383abd5bc7a17e5abb901408
AzumattDev <azumattgaming@gmail.com>
2023-10-09 08:44:30 -0400

    Update for Valheim 0.217.24

--- VersionHandshake.cs
+++ VersionHandshake.cs
@@ -54,8 +54,8 @@ namespace ItemManagerModTemplate
         {
             if (__instance.m_connectionFailedPanel.activeSelf)
             {
-                __instance.m_connectionFailedError.resizeTextMaxSize = 25;
-                __instance.m_connectionFailedError.resizeTextMinSize = 15;
+                __instance.m_connectionFailedError.fontSizeMax = 25;
+                __instance.m_connectionFailedError.fontSizeMin = 15;
                 __instance.m_connectionFailedError.text += "\n" + ItemManagerModTemplatePlugin.ConnectionError;
             }
         }
